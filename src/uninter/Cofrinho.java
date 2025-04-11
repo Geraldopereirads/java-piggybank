ef5211a2d013bf9f9e04322ec4e12d11b1856f02
Geraldopereirads <geraldo.pereirads@hotmail.com>
2025-04-10 22:22:41 -0300

    feat: added add and list functionality

--- src/uninter/Cofrinho.java
+++ src/uninter/Cofrinho.java
@@ -1,5 +1,31 @@
 package uninter;
 
+import java.util.ArrayList;
+
 public class Cofrinho {
+
+	private ArrayList<Moeda> MoedasList;
+
+	public Cofrinho() {
+		this.MoedasList = new ArrayList<>();
+	}
+
+	public void adicionar(Moeda add) {
+		this.MoedasList.add(add);
+	}
+
+	public void moedasList() {
+
+		if (this.MoedasList.isEmpty()) {
+			System.out.println("Cofrinho vazio!");
+			
+		}else {
+			
+			for (Moeda moeda : this.MoedasList) {
+				moeda.info();
+			}
+		}
+
+	}
 
 }
